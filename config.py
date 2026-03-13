055d31874452abfa167757e5f91bff7a993cce2f
sachinopboy2 <sachin25u7@gmail.com>
2026-03-13 19:05:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID = 1234567  # अपना API ID यहाँ डालें
-API_HASH = "your_api_hash" # अपना API HASH यहाँ डालें
-BOT_TOKEN = "your_bot_token" # BotFather वाला टोकन
-OWNER_ID = 7081885854 # आपकी आईडी
+API_ID = 20106942  # अपना API ID डालें
+API_HASH = "3bfe2013e4399af96d78640db6dcd601" # अपना API HASH डालें
+BOT_TOKEN = "8636826174:AAGYhPt7xlHcHHt2gUINgX-BE8fyX-BhiNI" # अपना बोट टोकन डालें
+OWNER_ID = 8639712935 # आपकी आईडी
