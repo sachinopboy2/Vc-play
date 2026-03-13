357e84ab381091e673bbf92076d9592a9687db00
sachinopboy2 <sachin25u7@gmail.com>
2026-03-13 18:58:10 +0530

    Simplify config.py by removing comments and unused lines
    Removed unused configuration lines and retained essential settings.

--- config.py
+++ config.py
@@ -1,16 +1,6 @@
 import os
 
-# Telegram API Details (my.telegram.org से लें)
 API_ID = 1234567  # अपना API ID यहाँ डालें
 API_HASH = "your_api_hash" # अपना API HASH यहाँ डालें
-
-# Bot Token (BotFather से लें)
-BOT_TOKEN = "your_bot_token"
-
-# Owner & Sudo Users
-OWNER_ID = 7081885854
-SUDO_USERS = [7081885854] # आप बाद में और ID जोड़ सकते हैं
-
-# Database (लॉगिन सेशन स्टोर करने के लिए)
-SESSION_NAME = "user_session"
-
+BOT_TOKEN = "your_bot_token" # BotFather वाला टोकन
+OWNER_ID = 7081885854 # आपकी आईडी
