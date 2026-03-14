8de5fb4df2fc2272b701f38ba90f3075eb616592
sachinopboy2 <sachin25u7@gmail.com>
2026-03-14 08:04:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-API_ID = 20106942  # अपना API ID डालें
-API_HASH = "3bfe2013e4399af96d78640db6dcd601" # अपना API HASH डालें
-BOT_TOKEN = "8636826174:AAGYhPt7xlHcHHt2gUINgX-BE8fyX-BhiNI" # अपना बोट टोकन डालें
-OWNER_ID = 8639712935 # आपकी आईडी
+BOT_TOKEN = "YOUR_BOT_TOKEN"
+
+OWNER_ID = 123456789
+BOT_NAME = "VCChat Bot"
+VERSION = "2.0"
