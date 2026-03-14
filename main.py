1829bd0dc55fb63cb5169bf30b14a30040b35b3b
sachinopboy2 <sachin25u7@gmail.com>
2026-03-14 08:17:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from telegram.ext import Application, CommandHandler
 from config import BOT_TOKEN
-from handlers.start import start
-from handlers.help import help_cmd
-from handlers.info import info
+
+from start import start
+from help import help_cmd
+from info import info
+
 
 def main():
 
     app = Application.builder().token(BOT_TOKEN).build()
 
+    # Commands
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_cmd))
     app.add_handler(CommandHandler("info", info))
 
-    print("VCChat Bot Started Successfully")
+    print("""
+==============================
+ VCCHAT BOT STARTED
+==============================
+Server : VPS
+Status : Running
+""")
 
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
