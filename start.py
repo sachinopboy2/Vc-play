5a417efa0916c7d66114064776541be88f564e82
sachinopboy2 <sachin25u7@gmail.com>
2026-03-14 08:17:26 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from telegram import Update
 from telegram.ext import ContextTypes
-from utils.banner import START_BANNER
+from banner import START_BANNER
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(START_BANNER)
